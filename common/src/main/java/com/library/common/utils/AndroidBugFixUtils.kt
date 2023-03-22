5197dd4825025e0ee2e2798be5e37690f50e9106
鱼香肉丝没有鱼 <754667850@qq.com>
2023-03-22 09:28:41 +0800

    1.新增修复输入框BUG工具类

---
+++ common/src/main/java/com/library/common/utils/AndroidBugFixUtils.kt
@@ -0,0 +1,48 @@
+package com.library.common.utils
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.library.common.base.BaseApplication
+import java.lang.reflect.Field
+
+/**
+ *
+ * 解决 Android 自身的 Bug
+ * @author :yangbw
+ * @date :2023/3/22
+ */
+@Suppress("unused")
+class AndroidBugFixUtils {
+
+    /**
+     * 解决 InputMethodManager 造成的内存泄露
+     *
+     * 使用方式：
+     * ```
+     * override fun onDestroy() {
+     *     AndroidBugFixUtils().fixSoftInputLeaks(this)
+     *     super.onDestroy()
+     * }
+     * ```
+     */
+    fun fixSoftInputLeaks(activity: Activity) {
+        val imm =
+            BaseApplication.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val leakViews = arrayOf("mLastSrvView", "mCurRootView", "mServedView", "mNextServedView")
+        for (leakView in leakViews) {
+            try {
+                val leakViewField: Field =
+                    InputMethodManager::class.java.getDeclaredField(leakView) ?: continue
+                if (!leakViewField.isAccessible) leakViewField.isAccessible = true
+                val view: Any? = leakViewField.get(imm)
+                if (view !is View) continue
+                if (view.rootView == activity.window.decorView.rootView) {
+                    leakViewField.set(imm, null)
+                }
+            } catch (_: Throwable) {
+            }
+        }
+    }
+}
